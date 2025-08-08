2be63a1c0e5ce4f3664d0021bf51864d31e9b819
Randon Hall <randonph02@gmail.com>
2025-08-08 00:14:56 -0700

    Added skeleton outlines for histogram plot_objects and methods for the HistPlot class.

---
+++ src/plotternova/plot_classes/hist.py
@@ -0,0 +1,182 @@
+from ..core import PlotBase
+from ..plot_objects import PointsLines, Hist
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+from ..utils import *
+from typing import List, Tuple
+
+
+class HistPlot(PlotBase):
+    """
+    Class for creating histogram plots with points, lines, or fill-between.
+    """
+    
+    def __init__(self,
+                 hist_type: str = "bar",
+                 normalize: bool = False,
+                 stack: bool = False,
+                 alpha: float = 1,
+                 ratio: bool = False,
+                 xlim: Tuple[float, float] | None = None,
+                 ylim: Tuple[float, float] | None = None,
+                 xticks: npt.ArrayLike | None = None,
+                 yticks: npt.ArrayLike | None = None,
+                 ylog_on: bool = False,
+                 xlabel: str = "x",
+                 ylabel: str = "Frequency",
+                 text_info: List[dict] | None = None,
+                 legend_settings: None | str | dict = "default_inside",
+                 grid_style: str | None = None,
+                 **kwargs):
+        """
+        Constructor for a HistPlot object. This plot specializes in generating plots for neatly 
+        displaying multiple histograms with various mechanisms for comparison appearance.
+
+        Parameters:
+        -----------
+          - xlim/ylim: 2-tuple of lower and upper x, y limits. Default 'None' will use auto limits.
+          - xticks/yticks: array-like object containing location of major ticks. Defualt ticks when
+            'None' passed
+          - xlog_on/ylog_on: boolean enabling or disabling logarithmic axes
+          - xlabel/ylabel: str the x and y labels. Can pass in latex formatted strings for mathprint
+          - text_info: list of dictionaries. Defaults to 'None' if no text to be displayed. Each 
+            dictionary contains information for each piece of text and has the following parameters:
+            * "text": string of the text to add to the plot
+            * "coords": 2-tuple of the coordinates on where to place the text on the axes
+            * any other kwargs to pass into ax.text() (see matplotlib ax.text() for full options)
+          - legend_settings: Can take 3 possible arguments:
+            * None: will not create a legend
+            * str: choose quick available legend presets. Default is "default_inside". See 
+              utils.decorate_legend() for full list of presets.
+            * dict: for full customization, can pass in desired kwargs as a dict for ax.legend()
+          - grid_style: Defaults to None which does not activate the grid. Otherwise, pass in a str
+            for various presets. See utils.grid_adjuster() for various preset types
+          - **kwargs: keyword arguments for the BasePlot constructor
+
+        Generates:
+        ----------
+            Skeleton figure with all of the various settings stored as attributes, ready for plotting
+            with HistPlot.plot()
+        """
+        # initialize first as instance of PlotBase class
+        super().__init__(**kwargs)
+
+        # initialize the figure (without anything plotted yet)
+        self._setup_fig()
+
+        # assign the BasicPlot attriubtes
+        # -------------------------------
+        # tick attributes
+        self.xlim = xlim; self.ylim = ylim
+        self.xticks = xticks; self.yticks = yticks
+        self.ylog_on = ylog_on
+
+        # grid attributes
+        self.grid_style = grid_style
+
+        # xy label attributes
+        self.xlabel = xlabel; self.ylabel = ylabel
+
+        # extra text attribute
+        self.text_info = text_info
+
+        # legend attriubtes
+        if legend_settings is not None:
+            self.legend_on = True
+            self.legend_settings = legend_settings
+        else:
+            self.legend_on = False
+
+        # initialize a datasets attribute to store data to plot
+        self._datasets = []
+
+        # initialize handles and labels for plotted objects
+        self.legend_handles = []; self.legend_labels = []
+
+
+    def add_data(self, x_data, y_data, label, object_type="pointslines", **kwargs):
+        """
+        Add data to the plot, with the option to choose the type/format the data should be plotted
+        in. Adds data in the form of the Hist, Points, and Line objects.
+        """
+        match object_type.lower():
+            case "hist":
+                self._datasets.append(Hist())
+
+            case "points" | "line" | "pointslines":
+                self._datasets.append(PointsLines(x_data, y_data, label, **kwargs))
+                self.legend_handles
+
+            case _:
+                print(f"Warning: {object_type} is an unkown data type! Check documentation for " +
+                      "allowed data types.")
+
+
+    def remove_data(self):
+        pass
+
+    def _stack_hists(self):
+        """
+        Stacks all of the stored histograms in self._datasets, plot each hist on top of each other.
+        """
+        ...
+
+    def _plot_ratio(self):
+        """
+        If a ratio plot is requested, this method handles plotting it. It is used in plot()
+        """
+        ...
+
+    def plot(self, export=False, file_name="default.png", **kwargs):
+        """
+        Creates and shows a plot in a separate window once the data has been added and the necessary
+        plot configurations have been made.
+        """
+        # ----------------------------------------------------------------
+        # plotting all of the added datasets
+        # ----------------------------------------------------------------
+        minx_arr = []; maxx_arr = []; miny_arr = []; maxy_arr = []
+        for dataset in self._datasets:
+            ...
+
+        # ----------------------------------------------------------------
+        # adjusting the axes settings
+        # ----------------------------------------------------------------
+        # compute the x and y limits to apply if no limits were provided
+        if self.xlim is None:
+            self.xlim = compute_auto_limits(minx_arr, maxx_arr)
+
+        if self.ylim is None:
+            self.ylim = compute_auto_limits(miny_arr, maxy_arr, log=self.ylog_on)
+
+        # apply the settings to the axes
+        self.apply_axes_settings(self.xlabel, self.ylabel, self.xlim, self.ylim, self.xlog_on, 
+                                 self.ylog_on)
+
+        # apply tick adjustments
+        tick_adjuster(self.ax)
+
+        # legend
+        if self.legend_on:
+            decorate_legend(self.ax, self.legend_handles, self.legend_labels,
+                            settings = self.legend_settings)
+
+        # extra text to place on the plot
+        if self.text_info is not None:
+            add_text(self.ax, self.text_info)
+
+        # grid
+        if self.grid_style:
+            print(self.grid_style)
+            grid_adjuster(self.ax, self.grid_style)
+
+        # optional saving of the figure
+        if export:
+            self.export(file_name=file_name, **kwargs)
+
+        plt.show()
+
+        # restore plotting defaults
+        mpl.style.use('default')
--- src/plotternova/plot_objects.py
+++ src/plotternova/plot_objects.py
@@ -1,10 +1,15 @@
 import numpy as np
+import numpy.typing as npt
 
 
 # every plot objets must have a draw method supplied with axes to be plotted on
 
 class PointsLines:
-    def __init__(self, xdata, ydata, label, **kwargs) -> None:
+    def __init__(self, 
+                 xdata, 
+                 ydata, 
+                 label,
+                 **kwargs) -> None:
         """
         Create points and/or lines plot objects that represent data.
         """
@@ -14,16 +19,88 @@ class PointsLines:
         # store **kwargs for plotting
         self.kwargs = kwargs
 
-    def draw(self, ax):
+    def draw(self, ax) -> None:
+        """
+        Draws the histrogram on the supplied axes of BasicPlot or HistPlot objects
+        """
         handle, = ax.plot(self.xdata, self.ydata, **self.kwargs)
         return handle
+
+
+class ErrorBar:
+    def __init__(self,
+                 xdata: npt.ArrayLike,
+                 ydata: npt.ArrayLike,
+                 yerr: npt.ArrayLike,
+                 label: str,
+                 xerr: npt.ArrayLike | None = None,
+                 **kwargs):
+        self.xdata = xdata; self.ydata = ydata
+        self.yerr = yerr
+        self.label = label
+        self.xerr = xerr if xerr is not None else None
+
+        # store **kwargs for plotting histograms
+        self.kwargs = kwargs
+
+    def draw(self, ax) -> None:
+        """
+        Draws the data +/- error points on the supplied axes on a BasicPlot object
+        """
+        ax.errorbar(self.xdata, self.ydata)
+
+
+class Hist:
+    def __init__(self, 
+                 data: npt.ArrayLike, 
+                 label: str, 
+                 bins: npt.ArrayLike,
+                 err: str | npt.ArrayLike | None = None, 
+                 **kwargs):
+        """
+        Creates histogram of data where the error can be supplied.
+        """
+        self.data = data
+        self.label = label
+        self.err = err
+        
+        # store the bin attributes
+        self.bins = bins
+
+        # store various other kwargs that go into plotting a histogram
+        self.kwargs = kwargs
+
+    def draw(self, ax) -> None:
+        """
+        Draws the histrogram on the supplied axes
+        """
+        handle, = ax.hist(self.data)
+
+    def divide_hists(self, other, ax) -> npt.ArrayLike:
+        """
+        Method to divide one histogram's values by another. Meant to be plotted in a ratio plot, 
+        below the main histogram plot, for example. Ensure both histograms have the same binning.
+        """
+        ...
+
+    def display_stats(self, ax, sigma: float = 1):
+        """
+        Method to display the location of the mean and the +/- x*sigma from the mean for one hist. 
+        """
+        ...
+
+
+
+class Step:
+    def __init__(self):
+        ...
 
 
 class FillBetween:
     pass
 
 
-class Hist:
+class Contour:
     pass
 
 
