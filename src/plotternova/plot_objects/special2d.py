2e762bd533a1b78e5af8268e5345eacc7ddbabdc
Randon Hall <randonph02@gmail.com>
2025-08-14 23:32:53 -0700

    Removed old plot_objects module and moved the remaining plot object classes into the plot_objects package

---
+++ src/plotternova/plot_objects/special2d.py
@@ -0,0 +1,21 @@
+"""
+Plot objects for speicalized 2d plot types. These include
+    - Vector/Flow fields
+    - Matrix plots
+    - Contour plots
+    - Heat maps
+"""
+
+class VecField:
+    pass
+
+class Contour:
+    pass
+
+
+class Matrix:
+    pass
+
+
+class HeatMap:
+    pass
